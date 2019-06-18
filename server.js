ca21f095eac42447b8504f7e4806acd101a37d80
Andy Lindsay <andydlindsay@gmail.com>
2019-06-18 10:19:18 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const villainsRouter = require('./routes/villains');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+// routes
+app.use('/villains', villainsRouter);
+
+app.listen(3000, () => {
+    console.log('app is listening on port 3000');
+});
