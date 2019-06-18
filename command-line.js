50bc0e09048b9bf315e3d343b1e2c126a8df4e83
Andy Lindsay <andydlindsay@gmail.com>
2019-06-18 09:57:25 -0700

    finished BREAD functionality from command line

--- command-line.js
+++ command-line.js
@@ -30,21 +30,45 @@ client.connect((err) => {
 
         case 'read':
             id = process.argv.slice(2)[1];
-            console.log(id);
+            client.query('SELECT * FROM villains WHERE id = $1', [id], (err, res) => {
+                if (err) throw err;
+                console.log(res.rows);
+                client.end();
+            });
             break;
+
         case 'edit':
-
+            id = process.argv.slice(2)[1];
+            const newAlias = process.argv.slice(2)[2];
+            client.query('UPDATE villains SET alias = $1 WHERE id = $2', [newAlias, id], (err) => {
+                if (err) throw err;
+                console.log('record updated');
+                client.end();
+            });
             break;
+
         case 'add':
-
+            const alias = process.argv.slice(2)[1];
+            const movie = process.argv.slice(2)[2];
+            client.query('INSERT INTO villains (alias, movie) VALUES ($1, $2);', [alias, movie], (err) => {
+                if (err) throw err;
+                console.log('record created');
+                client.end();
+            });
             break;
+
         case 'delete':
-
+            id = process.argv.slice(2)[1];
+            client.query('DELETE FROM villains WHERE id = $1;', [id], (err) => {
+                if (err) throw err;
+                console.log('record deleted');
+                client.end();
+            });
             break;
+
         default:
-
+            client.end();
             break;
-
 
     }
 
